1e4f0438da58e25f466cd1bcfda18a685bf9e734
micheleintrevado <michele.intrevado.pc@gmail.com>
2022-12-28 11:48:23 +0100

    sensors completed

--- sensors/main.py
+++ sensors/main.py
@@ -9,17 +9,17 @@ def main():
     system = System()
     client = mqtt.Client("ID1")
     client.on_publish = lambda client, userdata, mid: print("PUBLISH: ", mid)
-    client.connect("localhost")
+    client.connect("172.17.0.2")
     while True:
         system.publish(client)
 
-        for index in range(0, 2):
+        for index in range(0, 5):
             SolarPanel.simulate(index, client, system.day, system.weather)
 
-        for index in range(0, 1):
+        for index in range(0, 3):
             WindTurbine.simulate(index, client, system.wind)
 
-        time.sleep(10)
+        time.sleep(30)
 
 
 if __name__ == "__main__":
