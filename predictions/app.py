9d71d66da2d5dc7b5bd9dd6dc22ef66d9d02c9a0
micheleintrevado <michele.intrevado.pc@gmail.com>
2023-01-12 19:34:28 +0100

    prediction on node red done

--- predictions/app.py
+++ predictions/app.py
@@ -1,16 +1,16 @@
 import json
 from flask import Flask
-
 from components.model import Model
 
 app = Flask(__name__)
 
-
 @app.route('/predictions/solar/<value>', methods=['GET'])
 def index(value):
+    value = int(value)
+    value = 10 if value > 10 else value
+    value = 0 if value < 0 else value
     model = Model()
-    predicted_value = model.predict(int(value))
+    predicted_value = model.predict(value)
     return json.dumps(predicted_value)
 
-
-app.run()
+app.run(debug=True,host='172.20.0.105', port=5000)
